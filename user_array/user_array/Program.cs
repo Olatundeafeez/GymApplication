6511bbe086c3e5d14964e6d009e87df61ebf31e6
Olatundeafeez <olatundefolorunshoafeez@gmail.com>
2024-05-09 13:44:24 +0100

    user array

---
+++ user_array/user_array/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace array
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            string username = new Username()
+
+        }
+    }
+}
---
+++ user_array/user_array/Userclass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_array
+{
+    public class Userclass
+    {
+        //fields
+        string name;
+        string email;
+        string password;
+        string phone_number;
+        string address;
+        string gender;
+
+        //properties
+        public string Name { get; set; }  
+        public string Email { get; set; }
+        public string Password { get; set; }    
+        public string PhoneNumber { get; set; } 
+        public string Address { get; set; } 
+        public string Gender { get; set; }
+
+        //constructor
+        public Userclass(string name, string email, string password, string phone_number, string address, string gender)
+        {
+            name = Name;
+            email = Email;
+            password = Password;
+            phone
+
+        }
+        //method
+
+        public static void Mymethod()
+        {
+            Console.WriteLine("enter your name /n your name should not be less than 4 ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("enter your email ");
+            string email = Console.ReadLine();
+
+            Console.writeline("enter your password /n password must not be less than 8 letters ");
+            string password = console.readline();
+
+            console.writeline("enter your phone number ");
+            string phone_number = console.readline();
+
+            console.writeline("enter your address ");
+            string address = console.readline();
+
+            console.writeline("enter your gender ");
+            string gender = console.readline();
+        }
+ }
+     }
